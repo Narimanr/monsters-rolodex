a0f936d29f9283a3bbfa5619017494213bb630e6
Nariman <nariman.rajabi@gmail.com>
2020-02-05 22:51:03 +0330

    Updates

---
+++ precache-manifest.bbce1be2a878b437f7cb346cbcd9b887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e950275997bbe3c92813561783f33a48",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "28cbfb36da5617afa061",
+    "url": "/monsters-rolodex/static/css/main.981628d7.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28cbfb36da5617afa061",
+    "url": "/monsters-rolodex/static/js/main.975e97df.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
